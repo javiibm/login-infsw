94261876ea02a76552a0da2469af49171631dc36
javiibm <javiera.jibm@gmail.com>
2023-09-28 02:02:02 -0300

    funcionaidades terminadas

--- Login/login/core/views.py
+++ Login/login/core/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.contrib import admin
+from django.http import HttpResponse
 # Create your views here.
+
+def login_user(request):
+        if request.method == "POST":
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                redirect('/admin/')
+        # Redirect to a success page.
+            else:
+                messages.error(request, "No está en la base de datos")
+        # Return an 'invalid login' error message.
+        return render(request, 'core/login.html', {})
--- Login/login/login/urls.py
+++ Login/login/login/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include
+from core import views
 
 urlpatterns = [
+    path('', views.login_user),
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls'))
 ]
